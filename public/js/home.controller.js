c4c88bce6770ae05e0cde6deaff6d09bc7cae45f
oliverfei <ofei006@uottawa.ca>
2015-09-19 09:03:37 -0400

    Added sentiment bar

--- public/js/home.controller.js
+++ public/js/home.controller.js
@@ -14,3 +14,31 @@ function homeCtrl(SentimentService){
 	}
 
 }
+function setSentimentValue(value) {
+
+    //set color depending on how high the value is
+    var color;
+    if (value < 0.33) {
+        color = '#d9534f';//red
+    } else if (value < 0.66) {
+        color = '#f0ad4e';//yellow
+    } else {
+        color = '#5cb85c';//green
+    }
+
+    //set the border color of the sentiment bar
+    var bar = document.getElementById("vertical-sentiment-bar");
+    bar.style.borderColor = color;
+
+    //delete any sentiment bar styles in the header
+    var headStyle = document.getElementById('barAfterStyle');
+    if (headStyle != null)
+        document.head.removeChild(headStyle);
+
+    //add new sentiment bar style in the header
+    var barAfterStyle = document.createElement("style");
+    barAfterStyle.setAttribute('id', 'barAfterStyle');
+    barAfterStyle.innerHTML = "#vertical-sentiment-bar:after{content: '';display: block;position: absolute;left: 4px;bottom: 4px;width: 75%;border-radius: 3px; height: " + value * 100 + "%; background: " + color + ";";
+
+    document.head.appendChild(barAfterStyle);
+}
