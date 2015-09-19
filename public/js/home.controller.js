6b843ef296b91d01311e2f784c3f6faa54f8b7e3
Shubhagata Sengupta <shubsengupta@gmail.com>
2015-09-19 10:36:42 -0400

    added firebase comments

--- public/js/home.controller.js
+++ public/js/home.controller.js
@@ -12,8 +12,10 @@ function homeCtrl(SentimentService){
 			vm.postSentiment = data.data;
 		})
 	}
+    vm.init = initFirebase;
 
 }
+
 function setSentimentValue(value) {
 
     //set color depending on how high the value is
@@ -39,6 +41,50 @@ function setSentimentValue(value) {
     var barAfterStyle = document.createElement("style");
     barAfterStyle.setAttribute('id', 'barAfterStyle');
     barAfterStyle.innerHTML = "#vertical-sentiment-bar:after{content: '';display: block;position: absolute;left: 4px;bottom: 4px;width: 75%;border-radius: 3px; height: " + value * 100 + "%; background: " + color + ";";
-
+    
     document.head.appendChild(barAfterStyle);
+}
+
+function initFirebase() {
+  // CREATE A REFERENCE TO FIREBASE
+  var messagesRef = new Firebase('https://htn-chat.firebaseio.com/');
+
+  // REGISTER DOM ELEMENTS
+  var messageField = $('#messageInput');
+  var nameField = $('#nameInput');
+  var messageList = $('#example-messages');
+
+  // LISTEN FOR KEYPRESS EVENT
+  messageField.keypress(function (e) {
+    if (e.keyCode == 13) {
+      //FIELD VALUES
+      var username = nameField.val();
+      var message = messageField.val();
+
+
+      //SAVE DATA TO FIREBASE AND EMPTY FIELD
+      messagesRef.push({name:username, text:message});
+      messageField.val('');
+    }
+  });
+
+  // Add a callback that is triggered for each chat message.
+  messagesRef.limitToLast(10).on('child_added', function (snapshot) {
+    //GET DATA
+    var data = snapshot.val();
+    var username = data.name || "anonymous";
+    var message = data.text;
+
+    //CREATE ELEMENTS MESSAGE & SANITIZE TEXT
+    var messageElement = $("<li class='comment'>");
+    var nameElement = $("<strong class='label label-default'></strong>")
+    nameElement.text(username);
+    messageElement.text(message).prepend(nameElement);
+
+    //ADD MESSAGE
+    messageList.append(messageElement)
+
+    //SCROLL TO BOTTOM OF MESSAGE LIST
+    messageList[0].scrollTop = messageList[0].scrollHeight;
+  });
 }
