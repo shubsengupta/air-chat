18793b733bba526fad95423bc80095e1e73b35cf
Tanatorn Nantawinyoo <tanatornn96@gmail.com>
2015-09-19 04:10:28 -0400

    Sentiment analysis is now done

--- public/js/sentiment/sentiment.service.js
+++ public/js/sentiment/sentiment.service.js
@@ -3,6 +3,17 @@ angular
 	.service('SentimentService', SentimentService);
 
 SentimentService.$inject = ['$http'];
-function SentimentService($http){
 
+function SentimentService($http){
+	/**
+	 * Posts a comment to the server in order to get the 
+	 * Indico sentimental analysis
+	 * @param  {data : String} data input comment
+	 * @return {promise}      Returns a promise which will return the
+	 * sentimental score
+	 */
+	
+	this.post = function(data){
+		return $http.post('/comment', data);	
+	}
 }
--- routes/index.js
+++ routes/index.js
@@ -14,8 +14,10 @@ router.post('/comment', function (req, res,next) {
 	var data = req.body;
 	request.post({ url:'http://apiv2.indico.io/sentiment?key=' + apiKey, form: data }, function (error, response, body) {
   	if (!error && response.statusCode == 200) {
-    	console.log(body) // Show the HTML for the Google homepage. 
     	res.send(body);
+  	}
+  	if (error){
+  		res.sendStatus(400);
   	}
   	});
 });
